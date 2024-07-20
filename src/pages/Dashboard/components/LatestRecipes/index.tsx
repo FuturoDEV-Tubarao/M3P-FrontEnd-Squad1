502f46340bfc7b64e9c7c6b45e80f26bc82ce4ad
Your Name <milenaclararizzi@gmail.com>
2024-07-20 18:25:03 -0300

    filtro de receitas aplicado - gluten e lactose

--- src/pages/Dashboard/components/LatestRecipes/index.tsx
+++ src/pages/Dashboard/components/LatestRecipes/index.tsx
@@ -1,14 +1,95 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Checkbox, CheckboxContainer, Container, Filters, Title, TitleContainer } from "./styles";
+import { RecipesContext } from "../../../../context/RecipeContext";
+import { Recipes } from "../Recipes";
+import { RecipeContainer } from "../Recipes/styles";
 import api from "../../../../axios/axiosConfig";
 
+enum RecipeType {
+  MAIN_DISH = "MAIN_DISH",
+  APPETIZERS = "APPETIZERS",
+  DRINKS = "DRINKS",
+  BREAKFAST = "BREAKFAST",
+}
+
+interface Recipe {
+  id?: string;
+  title: string;
+  description: string;
+  ingredients: string;
+  preparationTime: string;
+  preparationMethod: string;
+  recipeType: RecipeType;
+  glutenFree: boolean;
+  lactoseFree: boolean;
+  origin: string;
+  votes?: Vote[];
+  // lastModifiedDate?: string;
+  createdDate?: string;
+  url?: string;
+  createdBy?: {
+    name: string;
+    id: string;
+  };
+}
+
+interface Vote {
+  note: number;
+  feedback: string;
+  recipeId: string;
+  createdBy: {
+    name: string;
+  };
+}
+
 export function LatestRecipes() {
-    const [selectedFilter, setSelectedFilter] = useState<string>("Todas");
+  const { recipes } = useContext(RecipesContext);
+  const [filteredRecipes, setFilteredRecipes] = useState<Recipe[]>([]);
+  const [glutenFree, setGlutenFree] = useState(false);
+  const [lactoseFree, setLactoseFree] = useState(false);
+  const [showAll, setShowAll] = useState(true);
+
+  const applyFilters = (recipes: Recipe[]) => {
+    let filtered = recipes;
+  
+    if (glutenFree && lactoseFree) {
+      filtered = filtered.filter(recipe => recipe.glutenFree && recipe.lactoseFree);
+    } else if (glutenFree) {
+      filtered = filtered.filter(recipe => recipe.glutenFree && !recipe.lactoseFree);
+    } else if (lactoseFree) {
+      filtered = filtered.filter(recipe => !recipe.glutenFree && recipe.lactoseFree);
+    } else {
+      filtered = recipes;
+    }
+  
+    setFilteredRecipes(filtered);
+  };
+
+  useEffect(() => {
+    applyFilters(recipes);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [recipes, glutenFree, lactoseFree, showAll]);
 
   const handleFilterChange = (filter: string) => {
-    setSelectedFilter(filter);
+    switch (filter) {
+      case "glutenFree":
+        setGlutenFree(!glutenFree);
+        setShowAll(false);
+        break;
+      case "lactoseFree":
+        setLactoseFree(!lactoseFree);
+        setShowAll(false);
+        break;
+      case "all":
+        setShowAll(true);
+        setGlutenFree(false);
+        setLactoseFree(false);
+        break;
+      default:
+        break;
+    }
   };
-  
+
   const [users, setUsers] = useState(0)
   useEffect(() => {
     async function fetchData(){
@@ -18,42 +99,46 @@ export function LatestRecipes() {
     }
     fetchData()
   }, [])
-
-    return (
-      <Container>
-        <TitleContainer>
-          <div>
-            <Title>Últimas Receitas</Title>
-            <p>Usuários Ativos: {users}</p>
-          </div>
-          <Filters>
-            <CheckboxContainer>
-              <Checkbox
-                type="checkbox"
-                checked={selectedFilter === "Todas"}
-                onChange={() => handleFilterChange("Todas")}
-              />
-              Todas as Receitas
-            </CheckboxContainer>
-            <CheckboxContainer>
-              <Checkbox
-                type="checkbox"
-                checked={selectedFilter === "Sem Glúten"}
-                onChange={() => handleFilterChange("Sem Glúten")}
-              />
-              Sem Glúten
-            </CheckboxContainer>
-            <CheckboxContainer>
-              <Checkbox
-                type="checkbox"
-                checked={selectedFilter === "Sem Lactose"}
-                onChange={() => handleFilterChange("Sem Lactose")}
-              />
-              Sem Lactose
-            </CheckboxContainer>
-          </Filters>
-        </TitleContainer>
-      </Container>
-    );
-  }
   
+  return (
+    <Container>
+      <TitleContainer>
+        <div>
+          <Title>Últimas Receitas</Title>
+          <p>Usuários Ativos: {users}</p>
+        </div>
+        <Filters>
+          <CheckboxContainer>
+            <Checkbox
+              type="checkbox"
+              checked={showAll}
+              onChange={() => handleFilterChange("all")}
+            />
+            Todas
+          </CheckboxContainer>
+          <CheckboxContainer>
+            <Checkbox
+              type="checkbox"
+              checked={glutenFree}
+              onChange={() => handleFilterChange("glutenFree")}
+            />
+            Sem Glúten
+          </CheckboxContainer>
+          <CheckboxContainer>
+            <Checkbox
+              type="checkbox"
+              checked={lactoseFree}
+              onChange={() => handleFilterChange("lactoseFree")}
+            />
+            Sem Lactose
+          </CheckboxContainer>
+        </Filters>
+      </TitleContainer>
+        <RecipeContainer>
+          {filteredRecipes.map(recipe => (
+            <Recipes key={recipe.id} recipe={recipe} />
+          ))}
+        </RecipeContainer>
+    </Container>
+  );
+}
--- src/pages/Dashboard/components/Recipes/index.tsx
+++ src/pages/Dashboard/components/Recipes/index.tsx
@@ -1,6 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faCalendarAlt } from "@fortawesome/free-solid-svg-icons";
-// import { faStar as farFaStar } from "@fortawesome/free-regular-svg-icons";
 import {
   Badge,
   Button,
@@ -10,59 +9,88 @@ import {
   Header,
   Image,
   Meta,
-  RecipeContainer,
   StarRating,
   Title,
-  // VoteButton,
 } from "./styles";
-import { useContext } from "react";
-import { RecipesContext } from "../../../../context/RecipeContext";
 import ImagemReceita from "../../../../assets/ImagemReceita.jpg";
-import { AuthContext } from "../../../../context/AuthContext";
 import { Link } from "react-router-dom";
 import { Votes } from "../../../../components/Votes";
 
-export function Recipes() {
-  const { recipes } = useContext(RecipesContext);
-  const { logado } = useContext(AuthContext);
+enum RecipeType {
+  MAIN_DISH = "MAIN_DISH",
+  APPETIZERS = "APPETIZERS",
+  DRINKS = "DRINKS",
+  BREAKFAST = "BREAKFAST",
+}
 
+interface Recipe {
+  id?: string;
+  title: string;
+  description: string;
+  ingredients: string;
+  preparationTime: string;
+  preparationMethod: string;
+  recipeType: RecipeType;
+  glutenFree: boolean;
+  lactoseFree: boolean;
+  origin: string;
+  votes?: Vote[];
+  // lastModifiedDate?: string;
+  createdDate?: string;
+  url?: string;
+  createdBy?: {
+    name: string;
+    id: string;
+  };
+}
+
+interface Vote {
+  note: number;
+  feedback: string;
+  recipeId: string;
+  createdBy: {
+    name: string;
+  };
+}
+
+interface RecipeCardProps {
+  recipe: Recipe;
+}
+
+export function Recipes({ recipe }: RecipeCardProps) {
   return (
-    <RecipeContainer>
-      {recipes.map((recipe) => (
-        <Card key={recipe.id}>
-          <Image src={ImagemReceita} alt={recipe.title} />
-          <Content>
-            <Header>
-              <Badge>{recipe.recipeType}</Badge>
-              <div>
-                <StarRating>{logado() && <Votes recipe={recipe} />}</StarRating>
-              </div>
-            </Header>
-            <Title>{recipe.title}</Title>
-            <Meta>
-              {recipe.createdBy && !!recipe.createdBy.name && (
-                <div>
-                  <FontAwesomeIcon icon={faUser} />
-                  {recipe.createdBy.name}
-                </div>
-              )}
-              <div>
-                <FontAwesomeIcon icon={faCalendarAlt} />
-                {recipe.createdDate
-                  ? new Date(recipe.createdDate).toLocaleDateString("pt-BR")
-                  : "Data não disponível"}
-              </div>
-            </Meta>
-            <Description>{recipe.description}</Description>
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-              <Button as={Link} to={`/recipe/${recipe.id}`}>
-                Ver Receita
-              </Button>
-              {/* {logado() && <VoteButton>Votar Receita</VoteButton>} */}
+      <Card key={recipe.id}>
+        <Image src={ImagemReceita} alt={recipe.title} />
+        <Content>
+          <Header>
+            <Badge>{recipe.recipeType}</Badge>
+            <div>
+              <StarRating>{<Votes recipe={recipe} />}</StarRating>
             </div>
-          </Content>
-        </Card>
-      ))}
-    </RecipeContainer>
+          </Header>
+          <Title>{recipe.title}</Title>
+          <Meta>
+            {recipe.createdBy && !!recipe.createdBy.name && (
+              <div>
+                <FontAwesomeIcon icon={faUser} />
+                {recipe.createdBy.name}
+              </div>
+            )}
+            <div>
+              <FontAwesomeIcon icon={faCalendarAlt} />
+              {recipe.createdDate
+                ? new Date(recipe.createdDate).toLocaleDateString("pt-BR")
+                : "Data não disponível"}
+            </div>
+          </Meta>
+          <Description>{recipe.description}</Description>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <Button as={Link} to={`/recipe/${recipe.id}`}>
+              Ver Receita
+            </Button>
+            {/* {logado() && <VoteButton>Votar Receita</VoteButton>} */}
+          </div>
+        </Content>
+      </Card>
   );
 }
