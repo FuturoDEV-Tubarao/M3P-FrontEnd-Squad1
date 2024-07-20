3fd606b5b6688c9439487ffe422e275ad56552c8
Your Name <milenaclararizzi@gmail.com>
2024-07-20 17:20:05 -0300

    Update index.tsx

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -5,7 +5,6 @@ import { Login } from "../pages/Login";
 import { Register } from "../pages/Register";
 import { Dashboard } from "../pages/Dashboard";
 import { NewRecipe } from "../pages/Dashboard/components/NewRecipe";
-import { EditRecipe } from "../pages/EditeRecipe";
 import { AuthContextProvider } from "../context/AuthContext";
 import { RecipePage } from "../pages/RecipePage";
 import { RecipesContextProvider } from "../context/RecipeContext";
@@ -28,11 +27,9 @@ export function Router() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/addrecipe" element={<NewRecipe />} />
-            <Route path="/editrecipe" element={<EditRecipe />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/editProfile" element={<UserForm />} />
             <Route path="/recipe/:id" element={<RecipePage />} />
-            {/* <Route path="/recipereview" element={<NewRecipeReview />} /> */}
 
             <Route element={<RotaPrivada />}>
               {/* adicionar as rotas privadas aqui, deixei elas fora para poder visualizar todas */}
