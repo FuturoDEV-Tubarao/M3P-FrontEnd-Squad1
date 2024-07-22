1d4d96ba9a6ed2ce605a302313662d73705e5a19
Your Name <milenaclararizzi@gmail.com>
2024-07-22 11:01:14 -0300

    Update index.tsx

--- src/pages/Dashboard/components/LatestRecipes/index.tsx
+++ src/pages/Dashboard/components/LatestRecipes/index.tsx
@@ -77,6 +77,7 @@ export function LatestRecipes() {
 
   useEffect(() => {
     applyFilters(recipes);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [recipes, glutenFree, lactoseFree, selectedCategory, showAll]);
 
   const handleFilterChange = (filter: string) => {
