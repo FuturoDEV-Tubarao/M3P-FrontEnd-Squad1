a0ad346dec169a247bade7dfe77a990950c3202d
Your Name <milenaclararizzi@gmail.com>
2024-07-22 10:47:13 -0300

    atualizacao do componente categoria

--- src/pages/Dashboard/components/Categories/index.tsx
+++ src/pages/Dashboard/components/Categories/index.tsx
@@ -1,49 +1,61 @@
-import semglutem from "../../../../assets/sem-gluten.png";
+import semglutem from "../../../../assets/sem-gluten.jpg";
 import { CategoriesContainer, CategoriesGrid, CategoryButton, CategoryCard, CategoryImage, CategoryOverlay, CategoryTitle, Title } from "./styles";
 
+enum RecipeType {
+  MAIN_DISH = "MAIN_DISH",
+  APPETIZERS = "APPETIZERS",
+  DRINKS = "DRINKS",
+  BREAKFAST = "BREAKFAST",
+}
 
-export function Categories() {
-    const categories = [
-        {
-          title: "Prato Principal",
-          image: semglutem,
-          alt: "Um prato com legumes e carne",
-        },
-        {
-          title: "Aperitivos",
-          image: semglutem,
-          alt: "Uma tigela de aperitivos com frutas e nozes",
-        },
-        {
-          title: "Bebidas",
-          image: semglutem,
-          alt: "Uma tábua com maçãs verdes e ervas",
-        },
-        {
-          title: "Café da Manhã",
-          image: semglutem,
-          alt: "Uma tigela de salada com abacate e tomates",
-        },
-      ];
-    
-    
-      return (
-        <CategoriesContainer>
-          <Title>Categorias Populares</Title>
-          <CategoriesGrid>
-            {categories.map((category, index) => (
-              <CategoryCard key={index}>
-                <CategoryImage src="category.image" alt="category.alt" />
-                <CategoryOverlay>
-                  <CategoryTitle>{category.title}</CategoryTitle>
-                  <CategoryButton>
-                    Todas as Receitas <i className="fas fa-arrow-right"></i>
-                  </CategoryButton>
-                </CategoryOverlay>
-              </CategoryCard>
-            ))}
-          </CategoriesGrid>
-        </CategoriesContainer>
-      );
-    
+interface CategoriesProps {
+  handleCategoryChange: (category: RecipeType) => void;
+}
+
+export function Categories({ handleCategoryChange }: CategoriesProps) {
+  const categories = [
+    {
+      title: "Prato Principal",
+      type: RecipeType.MAIN_DISH,
+      image: semglutem,
+      alt: "Um prato com legumes e carne",
+    },
+    {
+      title: "Aperitivos",
+      type: RecipeType.APPETIZERS,
+      image: semglutem,
+      alt: "Uma tigela de aperitivos com frutas e nozes",
+    },
+    {
+      title: "Bebidas",
+      type: RecipeType.DRINKS,
+      image: semglutem,
+      alt: "Uma tábua com maçãs verdes e ervas",
+    },
+    {
+      title: "Café da Manhã",
+      type: RecipeType.BREAKFAST,
+      image: semglutem,
+      alt: "Uma tigela de salada com abacate e tomates",
+    },
+  ];
+
+  return (
+    <CategoriesContainer>
+      <Title>Categorias Populares</Title>
+      <CategoriesGrid>
+        {categories.map((category, index) => (
+          <CategoryCard key={index}>
+            <CategoryImage src={category.image} alt={category.alt} />
+            <CategoryOverlay>
+              <CategoryTitle>{category.title}</CategoryTitle>
+              <CategoryButton onClick={() => handleCategoryChange(category.type)}>
+                Todas as Receitas <i className="fas fa-arrow-right"></i>
+              </CategoryButton>
+            </CategoryOverlay>
+          </CategoryCard>
+        ))}
+      </CategoriesGrid>
+    </CategoriesContainer>
+  );
 }
