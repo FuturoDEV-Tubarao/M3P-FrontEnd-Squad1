fd869c29cbe9e5148176336c15bf5f9e92687bcd
cavenati <140075906+ManoelCavenati@users.noreply.github.com>
2024-07-26 16:34:02 -0300

    Contexto de Usuário Atualizado

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -36,21 +36,47 @@ export const AuthContext = createContext({} as AuthContextType);
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const navigate = useNavigate();
   const [user, setUser] = useState<User | null>(null);
+  const [timer, setTimer] = useState<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
     const token = localStorage.getItem("token");
     const isLogado = localStorage.getItem("isLogado");
+    const lastLoginTime = localStorage.getItem("lastLoginTime");
 
-    if (storedUser && token && isLogado === "true") {
-      setUser(JSON.parse(storedUser));
+    //Cálculo do tempo decorrido desde o login
+    //Caso a pessoa atualize a página o tempo não será zerado, continuará de onde estava
+    if (storedUser && token && isLogado === "true" && lastLoginTime) {
+      const loginTime = parseInt(lastLoginTime, 10);
+      const currentTime = Date.now();
+      const elapsed = currentTime - loginTime;
+      const remainingTime = 15 * 60 * 1000 - elapsed;
+
+      //Se o tempo restante for positivo (remainingTime > 0), o usuário ainda tem tempo restante na sessão:
+      //Se o remainingTime for zero ou negativo, a sessão já expirou e o usuário é deslogado (logout()).
+      if (remainingTime > 0) {
+        setUser(JSON.parse(storedUser));
+        startLogoutTimer(remainingTime);
+      } else {
+        logout(); // Se o tempo já tiver expirado
+      }
     }
   }, []);
+
+  const startLogoutTimer = (delay: number) => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    const newTimer = setTimeout(() => {
+      navigate("/session-expired");
+      logout();
+    }, delay);
+    setTimer(newTimer);
+  };
 
   const login = async (data: LoginData) => {
     try {
       const credentials = btoa(`${data.email}:${data.password}`);
-
       const response = await api.post("/api/labfoods/v1/auth", data, {
         headers: {
           Authorization: `Basic ${credentials}`,
@@ -60,6 +86,8 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       if (response.data && response.status === 200) {
         localStorage.setItem("token", response.data.token);
         localStorage.setItem("isLogado", "true");
+        localStorage.setItem("lastLoginTime", Date.now().toString());
+
         const userData: User = {
           id: response.data.id,
           name: response.data.name,
@@ -67,7 +95,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
         };
         localStorage.setItem("user", JSON.stringify(userData));
         setUser(userData);
-
+        startLogoutTimer(15 * 60 * 1000); // Inicia o temporizador ao fazer login
         navigate("/");
       } else {
         alert("Não foi possível realizar o login");
@@ -81,8 +109,11 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     localStorage.removeItem("token");
     localStorage.removeItem("isLogado");
     localStorage.removeItem("user");
+    localStorage.removeItem("lastLoginTime");
     setUser(null);
-    navigate("/");
+    if (timer) {
+      clearTimeout(timer); // Limpa o timer ao deslogar
+    }
   };
 
   function logado() {
