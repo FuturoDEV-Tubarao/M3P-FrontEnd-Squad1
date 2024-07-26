a7ea96e17f98ed97def300d9df48be505bb08460
Your Name <milenaclararizzi@gmail.com>
2024-07-26 16:44:33 -0300

    atualizacao estilizacao de categorias

--- src/pages/Dashboard/components/Categories/styles.ts
+++ src/pages/Dashboard/components/Categories/styles.ts
@@ -1,6 +1,5 @@
 import styled, { keyframes } from "styled-components";
 
-// Efeito de pulse para a animação ao passar o mouse
 export const pulseAnimation = keyframes`
   0% { transform: scale(1); opacity: 1; }
   50% { transform: scale(1.1); opacity: 0.7; }
