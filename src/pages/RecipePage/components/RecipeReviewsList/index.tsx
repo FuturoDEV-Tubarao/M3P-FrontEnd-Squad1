9639cdba8eb92098e70c9f74e1fb4f24ec83fcf6
Your Name <milenaclararizzi@gmail.com>
2024-07-27 16:43:38 -0300

    Update index.tsx

--- src/pages/RecipePage/components/RecipeReviewsList/index.tsx
+++ src/pages/RecipePage/components/RecipeReviewsList/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   Button,
@@ -14,12 +14,14 @@ import {
   SectionTitle,
   VoteContainer,
   VoteContent,
+  VoteHeader,
 } from "./styles";
 import { NewRecipeReview } from "../NewRecipeReview";
 import { Votes } from "../../../../components/Votes";
 import { faPen } from "@fortawesome/free-solid-svg-icons";
 import { AuthContext } from "../../../../context/AuthContext";
 import { Recipe, RecipesContext } from "../../../../context/RecipeContext";
+import { Trash } from "phosphor-react";
 
 interface RecipeReviewsProps {
   recipe: Recipe;
@@ -29,7 +31,7 @@ export function RecipeReviewsList({ recipe }: RecipeReviewsProps) {
   const [showReviewPopup, setShowReviewPopup] = useState(false);
   const [updatedRecipe, setUpdatedRecipe] = useState(recipe);
   const { logado, user } = useContext(AuthContext);
-  const { fetchRecipeById } = useContext(RecipesContext);
+  const { fetchRecipeById, deleteVote } = useContext(RecipesContext);
 
   const shouldShowReviewSection =
     logado() && user && recipe.createdBy && user.id !== recipe.createdBy.id;
@@ -46,16 +48,41 @@ export function RecipeReviewsList({ recipe }: RecipeReviewsProps) {
     }
   };
 
+  useEffect(() => {
+    setUpdatedRecipe(recipe);
+  }, [recipe]);
+
+  const handleDelete = async (voteId: string | undefined) => {
+    if (!voteId) {
+      alert("ID do voto não disponível");
+      return;
+    }
+
+    try {
+      await deleteVote(voteId);
+      const updatedRecipeData = await fetchRecipeById(recipe.id!);
+      if (updatedRecipeData) {
+        setUpdatedRecipe(updatedRecipeData);
+      }
+    } catch (error) {
+      alert("Erro ao tentar excluir o voto");
+    }
+  };
+
   return (
     <Container>
       <ReviewsSection>
         <CustomerReviewsTitle>Avaliações de Clientes</CustomerReviewsTitle>
         <div>
-          <Votes recipe={updatedRecipe} />
+          {updatedRecipe.voteAvg !== undefined && (
+            <Votes note={updatedRecipe.voteAvg} />
+          )}
         </div>
         <ReviewText>
-          {!!updatedRecipe.votes && updatedRecipe.votes.length} avaliações
-          globais
+          {!!updatedRecipe.votes &&
+            (updatedRecipe.votes.length === 1
+              ? "1 avaliação global"
+              : `${updatedRecipe.votes.length} avaliações globais`)}
         </ReviewText>
         <Divider />
         {shouldShowReviewSection && (
@@ -78,11 +105,18 @@ export function RecipeReviewsList({ recipe }: RecipeReviewsProps) {
             {!!updatedRecipe.votes &&
               updatedRecipe.votes.map((voto, index) => (
                 <VoteContainer key={index}>
-                  <ReviewAuthor>{voto.createdBy.name}</ReviewAuthor>
+                  <VoteHeader>
+                    <ReviewAuthor>{voto.createdBy.name}</ReviewAuthor>
+                    {user && user.id === voto.createdBy.id && voto.id && (
+                      <button onClick={() => handleDelete(voto.id)}>
+                        <Trash size={24} color="red" />
+                      </button>
+                    )}
+                  </VoteHeader>
                   <p>{voto.feedback}</p>
                   <VoteContent>
                     <p>nota {voto.note}</p>
-                    <Votes recipe={updatedRecipe} />
+                    {voto.note !== undefined && <Votes note={voto.note} />}
                   </VoteContent>
                   <Divider />
                 </VoteContainer>
